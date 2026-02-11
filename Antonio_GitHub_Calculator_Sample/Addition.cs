2e063d017cfa0650087446fe8d45f1475a4418e0
furiojamaica18 <jamaicafurio123@gmail.com>
2026-02-11 20:25:08 +0800

    feat:createaddition

---
+++ Antonio_GitHub_Calculator_Sample/Addition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antonio_GitHub_Calculator_Sample
+{
+    public class Addition
+    {
+        public double addition(double num1, double num2) { return num1 + num2; }
+    }
+}
